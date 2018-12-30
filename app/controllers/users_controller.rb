430f61ecf25b2c778a3a8dc135b3a8f3696d1b88
Rebel <allesrebel@yahoo.com>
2018-12-30 12:41:07 -0800

    Fixed some edge cases for application sessions Added placeholder image for header + navigation bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save or @user.errors.messages[:email] == ["has already been taken"]
-      redirect_to auth.sign_in_path( email: @user.email, flash: {notice: "Please authenticate - to access account"} )
+      redirect_to auth.sign_in_path( email: @user.email, success: true )
     else  # User had other errors
       redirect_to register_path(email: params[:email], error: @user.errors.any?)
     end
